3803f37c17ed97a06ef5c8e496a9adee2ae28bec
thecodearcher <brian.iyoha@gmail.com>
2018-12-22 08:53:00 +0100

    Implemented pay with paypal plugin

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,37 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {  ToastController } from 'ionic-angular';
+import { PayPal, PayPalPayment, PayPalConfiguration } from '@ionic-native/paypal';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [PayPal]
 })
 export class HomePage {
+  amount: string = '50';
+  response: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(private payPal: PayPal, private toastCtrl: ToastController) {
 
   }
 
+  async pay() {
+    try {
+      await this.payPal.init({
+        PayPalEnvironmentProduction: 'YOUR_PRODUCTION_CLIENT_ID',
+        PayPalEnvironmentSandbox: 'AUtMZUTLHAx4RIdeoUvhluZuTzwcSF3A5OOdG91258_YjPKC5bpF-fhULdkSyDZ-TBx_pdGoyd367B9a'
+      });
+      await this.payPal.prepareToRender('PayPalEnvironmentSandbox', new PayPalConfiguration({
+      }));
+
+      let payment = new PayPalPayment(this.amount, 'USD', 'Description', 'sale');
+      this.response = await this.payPal.renderSinglePaymentUI(payment);
+      this.toastCtrl.create({ 'message': 'Payment Successful', 'closeButtonText': 'OK' })
+      console.log(this.response);
+    } catch (error) {
+      this.response = error;
+      console.error(error);
+    }
+  }
 }
+
